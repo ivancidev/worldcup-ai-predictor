f1254fa44d7ca8b402a0ee43839b147be1f87a75
ivancidev <herlanherbas@gmail.com>
2026-06-11 00:26:02 -0400

    chore: update config, gitignore, deps and proxy setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.api-sports.io",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
